aeeb8c965f6b05030121336f701a60795658f2b0
RaphaelLima <raphaelshoty@gmail.com>
2019-10-17 14:21:53 -0300

    uploading the project itself and the first request and response xml

---
+++ src/test/java/com/webservices/soap/soapWebService/SoapWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservices.soap.soapWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
